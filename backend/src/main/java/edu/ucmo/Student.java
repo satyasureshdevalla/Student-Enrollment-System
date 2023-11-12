4e8251b066b7c4931f990e5bc36d7a472975fa83
satyasureshdevalla <devallasatyasuresh@gmail.com>
2023-11-12 15:40:02 -0600

    feat: add student backend service

---
+++ backend/src/main/java/edu/ucmo/Student.java
@@ -0,0 +1,96 @@
+package edu.ucmo;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "student")
+public class Student {
+    @Id
+    private String id;
+
+    private String username;
+    private String password;
+    private String name;
+    private String email;
+    private String contact_details;
+    private List<String> schedule;
+    private String max_credits;
+    private String current_term;
+
+    public Student() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact_details() {
+        return contact_details;
+    }
+
+    public void setContact_details(String contact_details) {
+        this.contact_details = contact_details;
+    }
+
+    public List<String> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(List<String> schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getMax_credits() {
+        return max_credits;
+    }
+
+    public void setMax_credits(String max_credits) {
+        this.max_credits = max_credits;
+    }
+
+    public String getCurrent_term() {
+        return current_term;
+    }
+
+    public void setCurrent_term(String current_term) {
+        this.current_term = current_term;
+    }
+}
